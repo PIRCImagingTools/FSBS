0c791dfa7f7c2bb6878d8a40aa354103ecd2ae73
WilliamReynolds <wtr8@pitt.edu>
2024-01-24 22:45:49 -0500

    add return codes for testing

--- app/autorun.sh
+++ app/autorun.sh
@@ -2,10 +2,15 @@
 
 
 case "$1" in 
-terminal)
-bash  
-;;
-*)
-eval python3.7 /app/main.py "$@"
-;;
+    terminal)
+        bash  
+    ;;
+    
+    *)
+        eval python3.7 /app/main.py "$@"
+        cmd_exit="$?"
+        if [ "$cmd_exit" -ne 0 ]; then
+            exit "$cmd_exit"
+        fi
+    ;;
 esac
